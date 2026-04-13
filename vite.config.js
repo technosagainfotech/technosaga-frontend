982acaa346969b00c5f9ae226849dea02418d778
technosagainfotech <technosagainfotech@gmail.com>
2026-04-13 06:19:09 +0530

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const routes = [
-  '/',
-  '/about',
-  '/works',
-  '/contact',
-  '/gallery',
-  '/career',
-  '/blogs',
-  '/teams',
-  '/services/web-design-development',
-  '/services/digital-marketing',
-  '/services/bpo-services',
-  '/services/graphic-design',
-  '/services/photo-video-production',
-  '/services/app-development',
-  '/services/job-consultancy',
-  '/services/event-management',
-  '/services/live-streaming',
-  '/services/political-rallies-events',
-];
 
 export default defineConfig({
   plugins: [
     react(),
-
-    ViteImageOptimizer({
-      png: { quality: 80 },
-      jpeg: { quality: 80 },
-      webp: { quality: 80 },
-    }),
-
-    sitemap({
-      hostname: 'https://technosagainfotech.in',
-      dynamicRoutes: routes,
-    }),
   ],
-
   build: {
     rollupOptions: {
       output: {
@@ -54,7 +14,6 @@ export default defineConfig({
       },
     },
   },
-
   server: {
     port: 3000,
   },
